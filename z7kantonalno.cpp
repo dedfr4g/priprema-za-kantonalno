2b39d04c19c121237546b4730df11f8f420f9c1d
dedfr4g <azur2005@hotmail.com>
2023-03-25 13:41:51 +0100

    Create z7kantonalno.cpp

---
+++ z7kantonalno.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int d, s;
+	cout << "Unesi duzinu terena" << endl;
+	cin >> d;
+	cout << "Unesi sirinu terena" << endl;
+	cin >> s;
+	cout << "Sportista dok jednom obidje teren otrci " << (2 * d) + (2 * s) << " metara" << endl;
+	
+
+	return 0;
+  
+}
+
