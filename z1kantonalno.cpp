1ffcc87d7042e6d80048d49fe942b25a99baf0ac
dedfr4g <azur2005@hotmail.com>
2023-03-27 22:33:29 +0200

    Update z1kantonalno.cpp

--- z1kantonalno.cpp
+++ z1kantonalno.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
+
+int main() 
 {
-	// U zadatku pise n<=10 to je valjda greska treba biti n>=10 jer brojevi ispod 10 nemaju cifre desetice...
 	int a;
-	pocetak:
+    pocetak:
 	cout << "Unesi prirodan broj " << endl;
 	cin >> a;
 	if (a >= 100 || a < 10)
@@ -14,7 +14,7 @@ int main()
 	}
 	else
 	{
-		cout << "Cifra desetica je " << a / 10 << endl;
+		cout << "Broj bez cifre desetice je " << a % 10 << endl;
 	}
-	return 0;
+    return 0;
 }
