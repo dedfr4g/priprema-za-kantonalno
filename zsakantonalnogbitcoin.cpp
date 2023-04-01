7dc0c3bed1510d99a8a897f0be948fe76851c7e8
dedfr4g <azur2005@hotmail.com>
2023-04-01 23:26:45 +0200

    Create zsakantonalnogbitcoin.cpp
    Malom Denisu baka je dala 10 eura za sladoled i kolače. Denis je odlučio da ne želi jesti slatkiše nego da želi
    biti poduzetan pa je odlučio novac uložiti u kriptovalute. Njegov lični bankar dostavio mu je predviđanja
    cijene bitcoina za idućih N dana, a Denis je odlučio ulagati na sljedeći način:
    - prvo će odabrati jedan dan kada će uložiti svih 10 eura u bitcoin;
    - u jednom od dana nakon toga prodat će sve bitcoine za eure;
    - konačno, u jednom danu od dana nakon toga opet će kupiti bitcoine (i pritom potrošiti sve eure).
    Koliko je najviše bitcoina Denis može imati nakon izvršenja svoga plana?
    ulazni podaci
    U prvom redu se nalazi broj N (3 ≤ N ≤ 100 000), broj dana
    U svakom od sljedećih N redaka nalazi se jedan reakan broj zaokružen na 3 decimale, cijena bitcoina toga
    dana (u eurima). Cijena neće biti manja od 0,01 EUR ni veća od 100 000 EUR.
    izlazni podaci
    Ispišite jedan realan broj, najveći broj bitcoina koje Denis može imati po završetku svog plana. Rezultat će
    se smatrati tačnim ako odstupa od službenog za najviše 0,001.

---
+++ zsakantonalnogbitcoin.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	int novac = 10;
+	cin >> n;
+	vector<float> cijenabtc(n);
+	for (int u=0; u < n; u++)
+	{
+		cin >> cijenabtc[u];
+	}
+	if (n > 3)
+	{
+
+
+		float prvanajmanja = *min_element(cijenabtc.begin(), cijenabtc.end() - 2);
+		float prvikorak = 10 / prvanajmanja;
+		float trebanaci = prvanajmanja;
+		auto it = find(cijenabtc.begin(), cijenabtc.end(), trebanaci);
+		__int64 index = distance(cijenabtc.begin(), it);
+
+		auto startaj = next(cijenabtc.begin(), index);
+		float najveci = *max_element(startaj, cijenabtc.end() - 1);
+		float drugikorak = najveci * prvikorak;
+		float trebanacidrugi = najveci;
+		auto itdva = find(cijenabtc.begin(), cijenabtc.end(), trebanacidrugi);
+		__int64 drugiindex = distance(cijenabtc.begin(), itdva);
+		auto startajdva = next(cijenabtc.begin(), drugiindex);
+		float druganajmanja = *min_element(startajdva, cijenabtc.end());
+		float trecikorak = drugikorak / druganajmanja;
+		cout << trecikorak << endl;
+	}
+	else if (n == 3)
+	{
+		float prvanajmanja = *min_element(cijenabtc.begin(), cijenabtc.end() - 2);
+		float prvikorak = 10 / prvanajmanja;
+		float drugikorak = prvikorak * cijenabtc[1];
+		float trecikorak = drugikorak / cijenabtc[2];
+		cout << trecikorak << endl;
+	}
+	return 0;
+}
